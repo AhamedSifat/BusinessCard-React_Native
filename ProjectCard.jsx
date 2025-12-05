7fd64eb530f4fa7dff5c736612a51417755eb176
sifat1234 <ahahmedsifat111@gmail.com>
2025-12-05 16:51:31 +0600

    StyleSheet_API

--- ProjectCard.jsx
+++ ProjectCard.jsx
@@ -1,22 +1,26 @@
+import { StyleSheet } from 'react-native';
+
 const ProjectsCard = ({ name, image }) => {
   return (
     <View>
-      <Image
-        source={image}
-        style={{ height: 150, aspectRatio: 16 / 9, borderRadius: 10 }}
-      />
-      <Text
-        style={{
-          fontWeight: 'bold',
-          fontSize: 18,
-          marginTop: 10,
-          color: 'dimgray',
-        }}
-      >
-        {name}
-      </Text>
+      <Image source={image} style={styles.image} />
+      <Text style={styles.text}>{name}</Text>
     </View>
   );
 };
 
 export default ProjectsCard;
+
+const styles = StyleSheet.create({
+  image: {
+    height: 150,
+    aspectRatio: 16 / 9,
+    borderRadius: 10,
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginTop: 10,
+    color: 'dimgray',
+  },
+});
